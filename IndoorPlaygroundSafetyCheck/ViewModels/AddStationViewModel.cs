2078a832cc7ff960f5b573eecdb6e075e9c56d7d
ibrahim ozkan <ibrahimkuralozkan@gmail.com>
2024-05-23 13:47:34 +0200

    Rfid

--- IndoorPlaygroundSafetyCheck/ViewModels/AddStationViewModel.cs
+++ IndoorPlaygroundSafetyCheck/ViewModels/AddStationViewModel.cs
@@ -1,7 +1,101 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.ObjectModel;
+//using System.ComponentModel;
+//using System.IO;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
+//using System.Windows;
+//using System.Windows.Input;
+//using IndoorPlaygroundSafetyCheck.Commands;
+//using IndoorPlaygroundSafetyCheck.Data;
+//using IndoorPlaygroundSafetyCheck.Models;
+//using Microsoft.VisualBasic;
+//using QRCoder;
+
+//namespace IndoorPlaygroundSafetyCheck.ViewModels
+//{
+//    public class AddStationViewModel : INotifyPropertyChanged
+//    {
+//        private readonly Data.SafetyCheckContext _context = new Data.SafetyCheckContext();
+//        private string _stationName;
+
+//        public string StationName
+//        {
+//            get => _stationName;
+//            set
+//            {
+//                _stationName = value;
+//                OnPropertyChanged();
+//            }
+//        }
+
+//        public ObservableCollection<Station> Stations { get; private set; }
+
+//        public ICommand AddStationCommand { get; private set; }
+
+//        public AddStationViewModel()
+//        {
+//            Stations = new ObservableCollection<Station>(_context.Stations.ToList());
+//            AddStationCommand = new Commands.RelayCommand(AddStationExecute, CanAddStationExecute);
+//        }
+
+
+//        private bool CanAddStationExecute(object parameter)
+//        {
+//            return !string.IsNullOrWhiteSpace(StationName);
+//        }
+
+//        private void AddStationExecute(object parameter)
+//        {
+//            string inputRfidUid = Interaction.InputBox("Bitte benutzen Sie die Admin-RFID-Karte", "Admin RFID Verification", "");
+//            if (!IsAdminRfidUid(inputRfidUid))
+//            {
+//                MessageBox.Show("Invalid or unauthorized RFID UID.");
+//                return;
+//            }
+
+//            var newStation = new Station
+//            {
+//                Name = StationName,
+//                InsertTimeStamp = DateTime.Now,
+//                UpdatedBy = LoginViewModel.LoggedInUser.FullName,
+//                UpdateTimeStamp = DateTime.Now,
+//                InsertedBy = LoginViewModel.LoggedInUser.FullName
+
+//            };
+
+//            _context.Stations.Add(newStation);
+//            _context.SaveChanges(); // Save to get the Ident
+
+//            // Generate and save QR code
+
+
+//            Stations.Add(newStation);
+//            StationName = string.Empty; // Reset for next input
+//            OnPropertyChanged(nameof(StationName));
+//        }
+
+//        private bool IsAdminRfidUid(string rfidUid)
+//        {
+//            return _context.Employees.Any(e => e.RfidUid == rfidUid && e.Position == 1); // Assume 1 is Admin position identifier
+//        }
+
+
+//        public event PropertyChangedEventHandler PropertyChanged;
+//        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+//        {
+//            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+//        }
+//    }
+//}
+
+
+
+
+
+using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
@@ -9,14 +103,13 @@ using System.Windows.Input;
 using IndoorPlaygroundSafetyCheck.Commands;
 using IndoorPlaygroundSafetyCheck.Data;
 using IndoorPlaygroundSafetyCheck.Models;
-using Microsoft.VisualBasic;
-using QRCoder;
+using IndoorPlaygroundSafetyCheck.Views;
 
 namespace IndoorPlaygroundSafetyCheck.ViewModels
 {
     public class AddStationViewModel : INotifyPropertyChanged
     {
-        private readonly Data.SafetyCheckContext _context = new Data.SafetyCheckContext();
+        private readonly SafetyCheckContext _context = new SafetyCheckContext();
         private string _stationName;
 
         public string StationName
@@ -36,9 +129,8 @@ namespace IndoorPlaygroundSafetyCheck.ViewModels
         public AddStationViewModel()
         {
             Stations = new ObservableCollection<Station>(_context.Stations.ToList());
-            AddStationCommand = new Commands.RelayCommand(AddStationExecute, CanAddStationExecute);
+            AddStationCommand = new RelayCommand(AddStationExecute, CanAddStationExecute);
         }
-    
 
         private bool CanAddStationExecute(object parameter)
         {
@@ -47,32 +139,34 @@ namespace IndoorPlaygroundSafetyCheck.ViewModels
 
         private void AddStationExecute(object parameter)
         {
-            string inputRfidUid = Interaction.InputBox("Bitte benutzen Sie die Admin-RFID-Karte", "Admin RFID Verification", "");
-            if (!IsAdminRfidUid(inputRfidUid))
+            var inputDialog = new InputDialog();
+            if (inputDialog.ShowDialog() == true)
             {
-                MessageBox.Show("Invalid or unauthorized RFID UID.");
-                return;
+                string inputRfidUid = inputDialog.RfidUid;
+                if (!IsAdminRfidUid(inputRfidUid))
+                {
+                    MessageBox.Show("Invalid or unauthorized RFID UID.");
+                    return;
+                }
+
+                var newStation = new Station
+                {
+                    Name = StationName,
+                    InsertTimeStamp = DateTime.Now,
+                    UpdatedBy = LoginViewModel.LoggedInUser.FullName,
+                    UpdateTimeStamp = DateTime.Now,
+                    InsertedBy = LoginViewModel.LoggedInUser.FullName
+                };
+
+                _context.Stations.Add(newStation);
+                _context.SaveChanges(); // Save to get the Ident
+
+                // Generate and save QR code
+
+                Stations.Add(newStation);
+                StationName = string.Empty; // Reset for next input
+                OnPropertyChanged(nameof(StationName));
             }
-
-            var newStation = new Station
-            {
-                Name = StationName,
-                InsertTimeStamp = DateTime.Now,
-                UpdatedBy = LoginViewModel.LoggedInUser.FullName,
-                UpdateTimeStamp = DateTime.Now,
-                InsertedBy = LoginViewModel.LoggedInUser.FullName
-
-            };
-
-            _context.Stations.Add(newStation);
-            _context.SaveChanges(); // Save to get the Ident
-
-            // Generate and save QR code
-          
-
-            Stations.Add(newStation);
-            StationName = string.Empty; // Reset for next input
-            OnPropertyChanged(nameof(StationName));
         }
 
         private bool IsAdminRfidUid(string rfidUid)
@@ -80,7 +174,6 @@ namespace IndoorPlaygroundSafetyCheck.ViewModels
             return _context.Employees.Any(e => e.RfidUid == rfidUid && e.Position == 1); // Assume 1 is Admin position identifier
         }
 
-     
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
@@ -88,3 +181,4 @@ namespace IndoorPlaygroundSafetyCheck.ViewModels
         }
     }
 }
+
---
+++ IndoorPlaygroundSafetyCheck/Views/InputDialog.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace IndoorPlaygroundSafetyCheck.Views
+{
+    public partial class InputDialog : Window
+    {
+        public string RfidUid { get; private set; }
+        public string Message { get; set; }
+        public bool IsWarning { get; set; }
+
+        public InputDialog()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsWarning)
+            {
+                RfidUid = passwordBox.Password;
+                DialogResult = true;
+            }
+            else
+            {
+                DialogResult = false;
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
